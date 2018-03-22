9862f6e56281283739a2df3c1582bf421f713cb1
Moses <toby@MosesdeiMac-2.local>
2018-03-22 09:53:06 +0800

    Initial Commit

---
+++ SwiftTest01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest01
+//
+//  Created by Toby on 2018/3/22.
+//  Copyright © 2018年 Toby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
