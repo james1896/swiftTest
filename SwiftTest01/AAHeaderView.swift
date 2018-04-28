631baba7eb02bf2288c2ab71266faf8c770729cd
Toby <toby@MosesdeiMac-2.local>
2018-04-28 15:25:59 +0800

    first commit

---
+++ SwiftTest01/AAHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  AAHeaderView.swift
+//  SwiftTest01
+//
+//  Created by Toby on 2018/3/22.
+//  Copyright © 2018年 Toby. All rights reserved.
+//
+
+import UIKit
+
+class AAHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SwiftTest01/AATableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AATableViewCell.swift
+//  SwiftTest01
+//
+//  Created by Toby on 2018/3/22.
+//  Copyright © 2018年 Toby. All rights reserved.
+//
+
+import UIKit
+
+class AATableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
